1eb032ed0ed3dfd12b541014455e795a791c10e6
Nguyễn Văn Nam <43204587+Vannam98@users.noreply.github.com>
2019-10-05 08:27:57 +0700

    Nam_Update bổ sung

---
+++ app/src/main/java/com/example/appbangiay/SignupActivity.java
@@ -0,0 +1,29 @@
+package com.example.appbangiay;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+     Button sign_up;
+     EditText sign_up_email;
+     EditText sign_up_password;
+     EditText sign_up_password_confirm;
+
+
+     @Override
+     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+          super.onCreate(savedInstanceState, persistentState);
+     }
+     private  void DangKy(){
+
+          sign_up =(Button) findViewById(R.id.sign_in);
+          sign_up_email = (em)
+     }
+}
+
+
